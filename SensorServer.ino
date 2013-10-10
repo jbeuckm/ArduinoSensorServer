8023b5d957ebf2690545497b2e16aa00009a5897
joe beuckman <joebeuckman@gmail.com>
2013-10-09 23:29:50 -0500

    working on a request parser

--- SensorServer.ino
+++ SensorServer.ino
@@ -66,7 +66,11 @@ void loop() {
 
 void processRequest(EthernetClient client, String request) {
   
-  Serial.println(request);
+  String firstLine = getValue(request, '\n', 0);
+  String secondLine = getValue(request, '\n', 1);
+  
+  Serial.println(firstLine);
+  Serial.println(secondLine);
 
   // send a standard http response header
   client.println("HTTP/1.1 200 OK");
@@ -95,3 +99,18 @@ void outputPair(EthernetClient client, String label, String value) {
 }
 
 
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
