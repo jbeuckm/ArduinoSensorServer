576ee87f0dd1f837042602ebb7cdd18832a44c95
joe beuckman <joebeuckman@gmail.com>
2013-10-10 00:12:57 -0500

    trapping first line of request

--- SensorServer.ino
+++ SensorServer.ino
@@ -64,14 +64,20 @@ void loop() {
 
 
 
+
+void outputPair(EthernetClient client, String label, String value) {
+  client.println("\t\""+label+"\": "+value+",");
+}
+
+
+
+
 void processRequest(EthernetClient client, String request) {
   
-  String firstLine = getValue(request, '\n', 0);
-  String secondLine = getValue(request, '\n', 1);
-  
-  Serial.println(firstLine);
-  Serial.println(secondLine);
+  int idx = request.indexOf("\n");
+  Serial.println(request.substring(0, idx));
 
+  
   // send a standard http response header
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: application/json");
@@ -93,24 +99,3 @@ void processRequest(EthernetClient client, String request) {
   client.println("}");
 }
 
-
-void outputPair(EthernetClient client, String label, String value) {
-  client.println("\t\""+label+"\": "+value+",");
-}
-
-
-String getValue(String data, char separator, int index) {
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
-  }
-
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
